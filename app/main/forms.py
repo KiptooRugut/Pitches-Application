abfb3a1559995eee71d6da42d7962b3a6557f629
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-06 15:20:46 +0300

    it will create updateProfile class and Initialize the field types by passing in two parameters.

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required, Email, Length
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell Us About Yourself...',validators = [Required()])
+    submit = SubmitField('Submit')
