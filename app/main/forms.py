98c268dc778eea5e2c629fb6a753e16384b124ef
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-08 15:59:05 +0300

    it will define updateprofile, post and comment forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,21 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell Us About Yourself...',validators = [Required()])
     submit = SubmitField('Submit')
 
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Name', validators=[Required(), Length(1, 64)])
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
+    bio = TextAreaField('About...', validators=[Required(), Length(1, 100)])
+    submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    post_title = StringField('Pitch title', validators=[Required()])
+    post_category = SelectField('Pitch category',choices=[('Select a category','Select a category'),('Pickup lines', 'Pickup lines'),('Interview','Interview'),('Product','Product'),('Promotions','Promotions')], validators=[Required()])
+    post_content = StringField('What is in your mind?')
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Body', validators=[Required()])
+    submit = SubmitField('Submit')
