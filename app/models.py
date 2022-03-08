2f986303b35b8b567918d6206030937b9c827175
Kiptoo Rugut <mccrug97@gmail.com>
2022-03-08 15:30:05 +0300

    it will add the user classes, methods and parameters

--- app/models.py
+++ app/models.py
@@ -53,3 +53,30 @@ class Comment(db.Model):
         return author
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    post = db.relationship('Post', backref='user', lazy="dynamic")
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
